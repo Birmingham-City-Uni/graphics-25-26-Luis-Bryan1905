21ef5412b23cf2e4358ae890f2d0a1e585170a79
Luis Bryan <luis190503@gmail.com>
2026-01-26 17:58:52 +0000

    Complete main week 1 tasks

--- Labs/week1/Task2.cpp
+++ Labs/week1/Task2.cpp
@@ -4,6 +4,8 @@
 
 int main()
 {
+	int ResolutionMode = 2; // 1 = full resoultion, 2 = half resolution
+
 	std::string inputFilename = "../images/stanford_bunny.png";
 	std::string outputFilename = "output_bunny.png";
 
@@ -19,22 +21,44 @@ int main()
 	// If you'd like, you can use the setPixel function you wrote in the previous task.
 	// The code below reduces the brightness of the image by 0.5x, as an example.
 
-	for(int y = 0; y < height; ++y) 
-		for (int x = 0; x < width; ++x) 
-			for (int c = 0; c < 3; ++c) {
-				int pixelIdx = x + y * width;
-				imageBuffer[pixelIdx * 4 + c] *= 0.5;
-			}
+	//for(int y = 0; y < height; ++y) 
+	//	for (int x = 0; x < width; ++x) 
+	//		for (int c = 0; c < 3; ++c) 
+	//			{
+	//			int pixelIdx = x + y * width;
+	//			imageBuffer[pixelIdx * 4 + c] *= 0.5;
+	//		}
 
 	// Once you have tested this code, comment out the for loops above and try the following tasks:
-	// * Task 1: Try making a *negative* of the input image. Pixels that are bright in the input
-	//           should be dark in your output. 
+	// * Task 1: Try making a *negative* of the input image. Pixels that are bright in the input should be dark in your output. 
 	//           Hint: if the pixels ranged in value from 0 to 1, you could replace each pixel value (v) with (1 - v). 
 	//           but remember, the pixels have 8-bit unsigned values, so range from 0 to 255!
+	
+
+	//for(int y = 0; y < height; ++y) 
+	//	for (int x = 0; x < width; ++x) 
+	//		for (int c = 0; c < 3; ++c) 
+	//		{
+	//			int pixelIdx = x + y * width;
+	//			imageBuffer[pixelIdx * 4 + c] = (255 - imageBuffer[pixelIdx * 4 + c]); //makes a negative of the input image
+	//		}
+
+
 	// * Optional Task 2: Try downsampling this image to 1/2 resolution.
 	//           You can either just keep one in every 4 pixels, or better yet, average the pixels in each 2x2 block.
 	//           Hint: Be careful when averaging! You probably want to convert the pixel values to floating-point to
-	//           do the averaging maths.
+	//           do the averaging maths.s
+
+	width /= ResolutionMode;
+	height /= ResolutionMode;
+
+	for (int y = 0; y < height; ++y)
+		for (int x = 0; x < width; ++x)
+			for (int c = 0; c < 3; ++c) 
+			{
+				int pixelIdx = x + y * width;
+			}
+
 
 	int errorCode;
 	errorCode = lodepng::encode(outputFilename, imageBuffer, width, height);
